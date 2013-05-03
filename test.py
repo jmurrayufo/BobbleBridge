648290d2ef74b2bae23623ca3182732714737ef0
John Murray <jmurrayufo@gmail.com>
2013-05-03 16:46:22 -0600

    Pick tests

--- test.py
+++ test.py
@@ -1,10 +1,36 @@
 import os
+import pickle
+import cPickle
+import time
 
+GID = 0
 
+class Ship( ):
+   def __init__( self ):
+      global GID
+      self.ID = GID
+      GID += 1
 
+   def Complexer( self ):
+      self.complex = 4
+
+data = dict()
+
+data['type'] = 0xF0
+data['Enemies'] = list()
+
+for i in range(25):
+   data['Enemies'].append( Ship( ) )
+
+sT = time.time()
 with open('sizetest.txt', 'w') as fp:
-    fp.write('hello')
+   pickle.dump(data,fp,1)
 
-statinfo = os.stat('sizetest.txt')
-print statinfo
-print statinfo.st_size
+print time.time()-sT
+
+with open('sizetest.txt', 'r') as fp:
+   test = pickle.load(fp)
+
+print test['type']
+print test['Enemies']
+print type( test['Enemies'])
