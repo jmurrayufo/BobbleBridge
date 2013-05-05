a3d9a0d2e062a9db1d1c89637985c032f7b06c80
John Murray <jmurrayufo@gmail.com>
2013-05-05 00:15:42 -0600

    Ship update with obj Object

---
+++ obj/Obj.py
@@ -0,0 +1,20 @@
+
+
+class Obj( ):
+   def __init__( self, *args, **kargs):
+
+      self.ID = None
+
+      self.X = float()
+      
+      self.Y = float()
+
+      self.VX = float()
+
+      self.VY = float()
+
+      self.Mass = float()
+
+      self.Size = float()
+
+      self.Sprite = None
--- obj/Ship.py
+++ obj/Ship.py
@@ -1,8 +1,29 @@
 
-
 class Ship( ):
    def __init__( self ):
       self.ID = 0
+      
+      self.Name = 'DEFAULT'
 
-   def Complexer( self ):
-      self.complex = 4
+      # AI event driven actions will be stored here. 
+      self.Action = None
+
+      # Event driven orders will be kept here
+      self.Orders = None
+
+      # Fleet of ship (if there is one)
+      self.Fleet = None
+
+      # We will keep four basic dicts to determine all related stats of the given ship.
+
+      # Systems cover all basic functions of the ship (Engine, Sensors, Life Support etc etc)
+      self.Systems = dict()
+
+      # Weapons cover all offensive damage dealing weapons the ship has on board and equipped. 
+      self.Weapons = dict()
+
+      # Shields covers the 4 groups of shields, their status, and their frequencies
+      self.Shields = dict()
+
+      # Hull represents the physical state of the ship. Several components will be kept here.
+      self.Hull = dict()
