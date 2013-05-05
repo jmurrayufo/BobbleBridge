5a73f9d4d51f6a1bc5ecb544bd787c065ee7cf9b
John Murray <jmurrayufo@gmail.com>
2013-05-05 13:47:46 -0600

    Remote Client Tester

--- client/Client.py
+++ client/Client.py
@@ -2,14 +2,18 @@
 import socket
 import time
 
-HOST = 'localhost'    # The remote host
+HOST = '192.168.1.106'    # The remote host
 PORT = 56464              # The same port as used by the server
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.settimeout(10)
+s.settimeout(1)
 s.connect((HOST, PORT))
-for i in range(10):  
-   s.sendall('Hello, world'+str(i) )
-   data = s.recv(1024)
-   print 'Received', repr(data)
-   time.sleep(0.1)
+strOut = ''
+for i in range(4096):
+   strOut += 'a'
+# print strOut
+for i in range(10000):  
+   s.sendall(strOut)
+   data = s.recv(4096)
+   # print 'Received', repr(data)
+   # time.sleep(0.1)
 # s.close()
