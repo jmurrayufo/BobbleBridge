91b296192b6c820c97fad897d8b0b68aae9927f5
John Murray <jmurrayufo@gmail.com>
2013-05-06 02:05:30 -0600

    Network ideas and basis

--- obj/Funcs.py
+++ obj/Funcs.py
@@ -1,6 +1,3 @@
-import cPickle
-
-
 def getLenBytes( data ):
    # Calculate 4 bytes
    retVal = list()
@@ -11,20 +8,7 @@ def getLenBytes( data ):
       div = 2**( 8 * (i) )
       retVal.append( (data % mod)/div )
       data -= retVal[-1]
-   return retVal[::-1]
-
-   
-x = list()
-for i in range(2**24-1):
-   x.append(i)
-
-print "D ready"
-
-# print x
-dtype  = chr(1)
-data = cPickle.dumps(x,2)
-print len(data)
-dLen = getLenBytes( x )
-
-print dLen
-print 2**24-1
+   tmp = str()
+   for i in retVal[::-1]:
+      tmp += chr(i)
+   return tmp
