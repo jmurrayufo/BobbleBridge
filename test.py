993042a13a695adf4c9dff509431fabf59528386
John Murray <jmurrayufo@gmail.com>
2013-05-08 16:59:12 -0600

    Threading test code

--- test.py
+++ test.py
@@ -1,47 +1,14 @@
-import cPickle
+import threading
+import time
+
+def Foo( lockObj=None ):
+   myThread = threading.current_thread()
+   for i in range(100):
+      lockObj.acquire() 
+      print i
+      lockObj.release()
 
 
-def getLenBytes( data ):
-   # Calculate 4 bytes
-   retVal = list()
-   data = len(data)
-   tmp = 0
-   for i in range(4):
-      mod = 2**( 8 * (i+1) )
-      div = 2**( 8 * (i) )
-      retVal.append( (data % mod)/div )
-      data -= retVal[-1]
-   tmp = str()
-   for i in retVal[::-1]:
-      tmp += chr(i)
-   return tmp
-
-   
-x = list()
-for i in range(2**16):
-   x.append(i)
-
-print "D ready"
-
-# print x
-dtype  = chr(255)
-data = cPickle.dumps(x,2)
-print len(data)
-dLen = getLenBytes( x )
-
-data = dtype + dLen + data
-
-print len(data)
-
-chksum = 0
-for i in data:
-   chksum+= ord(i)
-   chksum%=256
-print chksum
-print "\n",ord(data[0])
-
-print "\n"
-
-x = "abcdefg"
-
-print x[5:-1]
+lockBox = threading.Lock()
+for i in range(5):
+   threading.Thread( target=Foo, kwargs=({'lockObj':lockBox}) ).start()
